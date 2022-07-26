0b124b51b4617384a0d454c3d9ead8e724af1d11
ulzcq <syj9913@ynu.ac.kr>
2022-07-27 03:26:29 +0900

    Feat: 컨버전 서비스 - ConversionService
    스프링은 개별 컨버터를 모아두고 묶어 편리하게 사용할 수 있는 기능을 제공 : ConversionService 인터페이스
    구현체인 DefaultConversionService를 사용하면 되며, ISP(인터페이스 분리원칙)에 근거하여 구현되어서 사용자는 꼭 필요한 메서드만 알면 된다.
    스프링은 내부에서 ConversionService를 사용해서 타입을 변환한다.
    
    Issues: #3

---
+++ src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,35 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ *  스프링은 개별 컨버터를 모아두고 이것들을 묶어서 편리하게 사용할 수 있는 기능을 제공
+ *  : ConversionService
+ */
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService() {
+        //등록
+        DefaultConversionService conversionService = new DefaultConversionService(); //ConversionService 구현체
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        //사용
+        assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+        assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+        String ipPortString = conversionService.convert(new IpPort("127.0.0.1", 8080), String.class);
+        assertThat(ipPortString).isEqualTo("127.0.0.1:8080");
+    }
+}
