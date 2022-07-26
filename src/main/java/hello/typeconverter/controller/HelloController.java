8ea2b607608620e509335a3f872a9f804fd63a6a
ulzcq <syj9913@ynu.ac.kr>
2022-07-27 02:16:23 +0900

    Feat: 스프링 타입 컨버터 소개
    HTTP 쿼리 스트링으로 전달되는 파라미터는 문자타입이다.
    스프링은 타입을 변수에 맞게 자동으로 변환해준다.
    
    Issues: #1

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,31 @@
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    /** 과거에는 개발자가 직접 타입을 변환해야 했었다. */
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data"); //문자 타입 조회
+        Integer intValue = Integer.valueOf(data); //숫자 타입으로 변경
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    /** 스프링은 HTTP 쿼리 스트링으로 전달되는 파라미터 문자값의 타입을 변수에 맞게 자동으로 변환해준다.
+     * @RequestParam
+     * @ModelAttribute
+     * @PathVariable
+     */
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
