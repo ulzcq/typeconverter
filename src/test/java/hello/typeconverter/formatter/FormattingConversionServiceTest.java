c9f5ea0233851f7cd8bc92635890455a8ca6aea7
ulzcq <syj9913@ynu.ac.kr>
2022-07-28 03:32:24 +0900

    Feat: 포맷터를 지원하는 컨버전 서비스
    컨버전 서비스에는 컨버터만 등록할 수 있지만, 포맷터를 지원하는 컨버전 서비스를 사용하면 컨버전 서비스에 포맷터를 추가할 수 있다.
    *동작원리는 깃 이슈 참고
    스프링이 제공하는 DefaultFormattingConversionService를 사용하여 포맷터를 등록하고 사용해보자.
    
    Issues: #7

---
+++ src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
@@ -0,0 +1,39 @@
+package hello.typeconverter.formatter;
+
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class FormattingConversionServiceTest {
+
+    @Test
+    void formattingConversionService() {
+
+        /**
+         * DefaultFormattingConversionService
+         * - FormattingConversionService(포맷터를 지원하는 컨버전 서비스) + 통화/숫자 관련 몇가지 기본 포맷터 추가 제공
+         * - ConversionService을 상속받으므로 컨버터,포맷터 모두 등록 가능
+         */
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+
+        //컨버터 등록
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        //포맷터 등록
+        conversionService.addFormatter(new MyNumberFormatter());
+
+        //컨버터 사용
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+        //포맷터 사용
+        assertThat(conversionService.convert(1000, String.class)).isEqualTo("1,000");
+        assertThat(conversionService.convert("1,000", Long.class)).isEqualTo(1000L);
+    }
+}
