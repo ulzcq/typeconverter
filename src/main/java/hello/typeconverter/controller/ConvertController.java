667c5ccd3d24d47f02acc9c073fad30210b0b697
ulzcq <syj9913@ynu.ac.kr>
2022-07-27 15:58:37 +0900

    Featt: 뷰 템플릿에 컨버터 적용하기
    타임리프는 렌더링 시에 컵버터를 적용할 수 있는 방법을 편리하게 지원한다.
    ${{...}} 를 사용하면 자동으로 컨버전 서비스를 사용해서 변환된 결과를 출력한다.
    스프링과 통합되어서, 우리가 등록한 컨버터들을 사용할 수 있다.
    *th:field는 자동으로 컨버전 서비스를 적용해주어서 ${...} 괄호 하나만 해도 적용이 된다.
    
    Issues: #5

---
+++ src/main/java/hello/typeconverter/controller/ConvertController.java
@@ -0,0 +1,52 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ConvertController {
+
+    /** 뷰 템플릿에 컨버터 적용하기 */
+    @GetMapping("/convert-view")
+    public String  converterView(Model model){
+        model.addAttribute("number",10000);
+        model.addAttribute("ipPort", new IpPort("127.0.0.1",8080));
+        return "converter-view";
+    }
+
+    /** 타임리프 컨버전 서비스 적용 확인
+     * GET: 객체 -> 문자(form)
+     */
+    @GetMapping("/convert/edit")
+    public String converterForm(Model model){
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        Form form = new Form(ipPort);
+
+        model.addAttribute("form", form);
+        return "converter-form";
+    }
+
+    /** 타임리프 컨버전 서비스 적용 확인
+     * POST: 문자(from) -> 객체
+     */
+    @PostMapping("/convert/edit")
+    public String converterEdit(@ModelAttribute Form form, Model model) {
+        IpPort ipPort = form.getIpPort();
+        model.addAttribute("ipPort", ipPort);
+        return "converter-view";
+    }
+
+    @Data
+    static class Form {
+        private IpPort ipPort;
+
+        public Form(IpPort ipPort) {
+            this.ipPort = ipPort;
+        }
+    }
+}
