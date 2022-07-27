8430210f79e2469f2ad3e1ff422349a9d447fb01
ulzcq <syj9913@ynu.ac.kr>
2022-07-28 03:44:09 +0900

    Feat: 포맷터 적용하기
    포맷터를 WebConfig에 등록해서 웹 애플리케이션에 적용해보고, 정상작동하는지 테스트 해보자
    /converter-view (객체 -> 문자)
    /hello-v2?data=10,000 (문자 -> 객체)
    
    Issues: #7

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,11 +16,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         /** 스프링은 내부에서 수 많은 기본 컨버터들을 제공해서 자동 적용된다 */
+        //우선순위 때문에 주석처리(컨버터 > 포맷터)
 //        registry.addConverter(new StringToIntegerConverter());
 //        registry.addConverter(new IntegerToStringConverter());
 
         //사용자 정의 컨버터
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
